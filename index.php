7b1ef544a29e13600d5f3ab30974598d5d185a52
mrzalais <lejnieksedijs@gmail.com>
2023-08-30 22:56:17 +0300

    Add necessary boilerplate files

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+use App\ApiClient;
+use App\Cache;
+use App\Enums\Size;
+use App\Services\DataFormattingService;
+use GuzzleHttp\Client;
+
+require 'vendor/autoload.php';
+
+$guzzleClient = new Client([
+    'base_uri' => 'https://api.printful.com/',
+]);
+
+$apiClient = new ApiClient($guzzleClient, new DataFormattingService());
+$cache = new Cache(__DIR__ . '/cache');
+
+$productId = 438;
+$size = Size::Large->value;
+$cacheKey = "product_{$productId}_size_{$size}";
+
+$cachedData = $cache->get($cacheKey);
+
+if ($cachedData !== null) {
+    $productAndSizeData = $cachedData;
+} else {
+    $productAndSizeData = $apiClient->getProductAndSizeData($productId, $size);
+
+    // Store data in cache for 5 minutes
+    $cache->set($cacheKey, $productAndSizeData, 300);
+}
+
+print_r($productAndSizeData);
